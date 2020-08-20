848564ca9aaacd936904beb0afe405ba6a7b1654
jiveshs98 <jiveshs98@hotmail.com>
2020-08-20 20:52:36 +0530

    Integrated Payment Gateway: PayTm

--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -7,6 +7,11 @@ from math import ceil
 
 import json
 
+from django.views.decorators.csrf import csrf_exempt
+
+from PayTm import Checksum
+MERCHANT_KEY = '8f#g0#HshiIcd_b8'
+
 # Create your views here.
 
 def index(request):
@@ -98,6 +103,7 @@ def checkout(request):
     if request.method == "POST":
         items_json = request.POST.get('itemsJson','')
         name = request.POST.get('name','')
+        amount = request.POST.get('amount','')
         email = request.POST.get('email','')
         address = request.POST.get('address1','') + " " + request.POST.get('address2','')
         city = request.POST.get('city','')
@@ -106,7 +112,7 @@ def checkout(request):
         phone = request.POST.get('phone','')
 
         # Adding data to the model
-        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, phone=phone, amount=amount)
         order.save()
 
         # Provide update once order is placed
@@ -116,7 +122,45 @@ def checkout(request):
         # Provide final message for placed order
         thank = True
         oid = order.order_id
-        return render(request,'shop/checkout.html',{'thank':thank, 'id':oid})
+        #return render(request,'shop/checkout.html',{'thank':thank, 'id':oid})
+
+        # Request Paytm to transfer the amount to your account after payment by user
+        param_dict = {
+            "MID": "ybZfqf76628328941070",
+            "ORDER_ID": str(order.order_id),
+            "CUST_ID": email,
+            "TXN_AMOUNT": str(amount),
+            "CHANNEL_ID": "WEB",
+            "INDUSTRY_TYPE_ID": "Retail",
+            "WEBSITE": "WEBSTAGING",
+            "CALLBACK_URL": "http://127.0.0.1:8000/shop/handlerequest/",
+        }
+
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+        
+        return render(request,'shop/paytm.html',{'param_dict':param_dict})
 
     return render(request,'shop/checkout.html')
     # return HttpResponse("We are at checkout page.")
+
+
+@csrf_exempt
+def handlerequest(request):
+    # Paytm will send you post request here. Exempting CSRF for this purpose
+    form = request.POST
+    print(form.keys())
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+            
+
+    # Verify Checksum through Paytm POST Request
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('Order Successful')
+        else:
+            print("Order was not successful because " + response_dict['RESPMSG'])
+    return render(request,'shop/paymentstatus.html', {'response':response_dict})
